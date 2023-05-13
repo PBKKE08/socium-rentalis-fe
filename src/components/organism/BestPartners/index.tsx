8649c74f5135100b49fcd6794b8a83ad5f8238ad
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-05-14 00:54:20 +0700

    edit interface

--- src/components/organism/BestPartners/index.tsx
+++ src/components/organism/BestPartners/index.tsx
@@ -40,7 +40,7 @@ export default function BestPartners({ className }: BestPartnersProps) {
     <div className={className}>
       <h1 className="section-heading mb-3">Best Partners</h1>
       <p className="text-font-primary-400 mb-10">Always there when needed</p>
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex justify-center items-center gap-8 md:gap-4 flex-col md:flex-row flex-wrap lg:flex-nowrap">
         {partnersData.map((partner, index) => (
           <PartnerCard
             category={partner.category}
