da6300a16dbb79b0dc88b20dd66b869e9277f7a9
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-05-25 11:05:03 +0700

    create preregister partner

--- src/pages/partners/pre-register/index.tsx
+++ src/pages/partners/pre-register/index.tsx
@@ -4,15 +4,28 @@ import Link from "next/link";
 
 export default function PreRegister() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-8 py-16">
+    <div className="flex flex-col items-center justify-center min-h-screen px-8 py-16 w-full md:w-1/2 mx-auto">
       <Link href="/">
         <LogoIcon isBig />
       </Link>
 
       <h1 className="text-heading text-4xl font-semibold">Partner Register</h1>
-      <p className="text-base text-font-primary-400 mt-3">
+      <p className="text-base text-font-primary-400 mt-3 mb-10">
         Register if you are not partner.
       </p>
+      <div className="flex flex-col w-full">
+        <h2 className="text-heading font-medium text-2xl mb-3 text-center">
+          Syarat dan Ketentuan
+        </h2>
+        <ul className="text-base text-font-primary-400 mt-3 mb-10 list-disc">
+          <li>Berumur minimal 18 tahun</li>
+          <li>
+            Jika membatalkan orderan lebih dari 3 kali, maka akun akan di ban
+            secara permanen
+          </li>
+          <li>Upah diperoleh dengan potongan 10% pada setiap pemesanan</li>
+        </ul>
+      </div>
 
       <Button isPrimary href="/partners/register" className="mb-3">
         Continue to Register
---
+++ src/pages/partners/register/index.tsx
@@ -0,0 +1,3 @@
+export default function PartnerRegister() {
+  return <div>PartnerRegister</div>;
+}
