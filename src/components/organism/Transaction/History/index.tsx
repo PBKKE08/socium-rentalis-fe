ec35cf4fbb28ef5b3a16c400efa4141bcf56476e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-06-21 22:42:57 +0700

    upcoming

--- src/components/organism/Transaction/History/index.tsx
+++ src/components/organism/Transaction/History/index.tsx
@@ -10,7 +10,7 @@ export default function History({ className, histories }: HistoryProps) {
       <div className="flex flex-col justify-center items-center gap-8">
         {histories.length > 0 ? (
           histories.map((item: any) => (
-            <CardHistoryTransaction key={item.id} data={item} />
+            <CardHistoryTransaction key={item.order_id} data={item} />
           ))
         ) : (
           <div className="text-center text-2xl">No upcoming transaction</div>
